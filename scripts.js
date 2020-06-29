713bef0c6ec18e7d5135e55f20db11b26a8a58b2
Quinten <37619249+Quintenn@users.noreply.github.com>
2020-06-29 23:45:11 +0200

    removed console.logs

--- scripts.js
+++ scripts.js
@@ -6,10 +6,8 @@
 
 //COVIDAPI
 $.getJSON("https://api.covid19api.com/summary", function (data) {
-    console.log(data);
     var cases = data.Global.TotalConfirmed,
         advice = document.getElementById('advice').innerHTML;
-    console.log(cases);
     $('.cases').append(cases);
     if (cases>=20000){
         document.getElementById('advice').innerHTML="Because of the numerous cases of COVID-19 in this country, it is not recommended as a landing spot.";
@@ -33,7 +31,6 @@ $.getJSON("https://api.covid19api.com/summary", function (data) {
 
 //WEATHERAPI
 $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=Madrid&units=metric&appid=f1f72acbd4642e54bfd47e688bcb8f51", function (data) {
-    console.log(data);
     var icon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png",
         weather = data.weather[0].main,
         temp = Math.floor(data.main.temp),
@@ -155,8 +152,10 @@ $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=Madrid&units=metric
 //
 //De gebruiker gebruikt de dropdown om een land te selecteren. Wanneer een land wordt geselecteerd maakt het script een oproep naar de 2 API's om het weer op die locatie en de informatie ivm COVID-19 in dat land te //vinden. Nadat die data is opgeroept wordt er een waarde aan toegehecht die de kwaliteit van de landingsspot bepaalt. Bijvoorbeeld: Gebruiker kiest Nederland en het systeem zoekt alle informatie op. Het blijkt dat //de weersituatie een kwaliteit van 2 heeft (slecht) en dat de Corona situatie in Nederland best goed is en dus een waarde van 4 heeft (goed). Het systeem pakt dan die twee waardes en neemt het gemiddelde ervan om de //algemene kwaliteit van de landingplaats te krijgen. In dit geval is dat 3 en dan geeft het systeem aan de gebruiker aan dat dit een middelmatige landingplaats is en legt uit waarom. 
 //
-//Één van de redenen dat dit niet volledig is gelukt is als volgt: Ik kon niet goed de waardes van allebei de API's samenvoegen om 1 waarde te vormen. Dit kwam vooral door wat problemen met global variables en variables in een functie. Ik probeerde dit nog te fixen door 2 API's in 1 functie op te roepen (zie regel 88) maar dit bleek met de tijd die ik had niet mogelijk te zijn.
+//Één van de redenen dat dit niet volledig is gelukt is als volgt: Ik kon niet goed de waardes van allebei de API's samenvoegen om 1 waarde te vormen. Dit kwam vooral door wat problemen met global variables en variables in een functie. Ik probeerde dit nog te fixen door 2 API's in 1 functie op te roepen (zie regel 79) maar dit bleek met de tijd die ik had niet mogelijk te zijn.
 //
 //Een andere rede is omdat ik niet wist hoe ik de namen in de dropdown lijst moest linken aan de locatiedata van allebei de API's. Vandaar ook dat de app nu standaard zoekt naar het weer in Madrid en naar globale COVID-19 statistieken.
 //
 //In principe werkt het systeem wel, maar alleen om het weer in Madrid te laten zien, de wereldwijde COVID-19 confirmed cases, en advies dat alleen gebaseerd is op die cases en niet op het weer.
+//
+//Note: blijkbaar werkt de pages versie ook niet omdat het openweathermap http doet en niet https. de live preview in brackets doet het wel goed.
